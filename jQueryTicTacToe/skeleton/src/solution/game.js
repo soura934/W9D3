10ce28261a4b4b6d2d4ea9223c50ab1616153a3f
Stephen <syangrea@gmail.com>
2020-12-30 16:06:19 -0500

    display game logic

--- jQueryTicTacToe/skeleton/src/solution/game.js
+++ jQueryTicTacToe/skeleton/src/solution/game.js
@@ -12,6 +12,7 @@ class Game {
   }
 
   playMove(pos) {
+    // debugger
     this.board.placeMark(pos, this.currentPlayer);
     this.swapTurn();
   }
--- jQueryTicTacToe/skeleton/src/ttt-view.js
+++ jQueryTicTacToe/skeleton/src/ttt-view.js
@@ -3,6 +3,7 @@ class View {
     this.game = game;
     this.$board = $el;
     this.setupBoard();
+    this.bindEvents();
   }
   bindEvents() {
     $('ul').on('click', 'li', (event) => {
@@ -11,12 +12,14 @@ class View {
   }
 
   makeMove($square) {
+    // debugger
     if ($square.hasClass('click')) {
       alert("invalid move try again")
     } else {
+      // debugger
       let i = $('li').index($square[0]);
       let player = this.game.currentPlayer;
-      this.game.playMove([i % 3, i / 3]);
+      this.game.playMove([i % 3, Math.floor(i / 3)]);
       $square.append(player);
       $square.addClass('click');
     }
